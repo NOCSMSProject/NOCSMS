67b87932e98feb1c2a434c868859ef3b2844e33a
unknown <yyj6655@yyj6655.tcent.cn>
2014-03-10 17:30:57 +0800

    项目初始化

---
+++ NOCSMS.Web/App_Code/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+///BasePage 的摘要说明
+/// </summary>
+public class BasePage : System.Web.UI.Page
+{
+    public const string userName = "userName";
+	
+    public BasePage()
+	{
+        
+        if (Session[userName] == null)
+        {
+            HttpContext.Current.Response.Redirect("Login.aspx");
+        }
+	}
+}
---
+++ README.txt
@@ -0,0 +1,2 @@
+ 机房安全管理系统（NetworkOperatingCenterSecurityManagementSystem--NOCSMS）
+ 2014-3-10 初始建立，以简单的三层框架作为基础设计，便于团队开发
